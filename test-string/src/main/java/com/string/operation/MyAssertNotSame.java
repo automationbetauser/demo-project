e05e7f76462606b596e57f071f2a0b160dbf90a8
Priyank Agarwal <apriyank@vmware.com>
2018-06-20 14:11:31 +0530

    changing structure of project

---
+++ test-string/src/main/java/com/string/operation/MyAssertNotSame.java
@@ -0,0 +1,16 @@
+package com.string.operation;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MyAssertNotSame {
+
+    public String getPropValue(final String key){
+        Map<String, String> appProps = new HashMap<String, String>();
+        appProps.put("key1", "value 1");
+        appProps.put("key2", "value 2");
+        appProps.put("key3", "value 3");
+        return appProps.get(key);
+    }
+
+}
