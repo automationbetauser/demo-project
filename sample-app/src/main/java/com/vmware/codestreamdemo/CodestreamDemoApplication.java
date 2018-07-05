1335a6d73bb075e4c69b9dcf8cb0e31d8ececaca
Aditi Srinivas <aditis@vmware.com>
2018-07-05 05:30:10 -0700

    Update CodestreamDemoApplication.java

--- sample-app/src/main/java/com/vmware/codestreamdemo/CodestreamDemoApplication.java
+++ sample-app/src/main/java/com/vmware/codestreamdemo/CodestreamDemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CodestreamDemoApplication {
-    //Test
+    //Test..
 	public static void main(String[] args) {
 		SpringApplication.run(CodestreamDemoApplication.class, args);
 	}
