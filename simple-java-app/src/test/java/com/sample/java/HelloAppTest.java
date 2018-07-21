90da3fef5a3af64c1a3df6ae7aed4b92cfa9c7a0
Automation BetaUser <automationbetauser@gmail.com>
2018-07-21 13:19:26 +0000

    Update HelloAppTest.java

--- simple-java-app/src/test/java/com/sample/java/HelloAppTest.java
+++ simple-java-app/src/test/java/com/sample/java/HelloAppTest.java
@@ -20,33 +20,4 @@ public class HelloAppTest {
         String[] args = {"1"};
         HelloApp.main(args);
     }
-
-    @Test
-    public void testWrongArgument() {
-        String[] args = {"bicycle"};
-        assertThat(HelloApp.main(args), is(equalTo("I don't understand the parameter you passed me. Is it a number? " +
-                "Parameter was: [" + args[0] + "]")));
-    }
-
-    @Test
-    public void testHelloError() throws Exception {
-        String[] args = {"5"};
-        assertNotEquals(HelloApp.main(args), "Hello\\nHello\\nHello\\nHello\\nHello\\n");
-    }
-
-    @Test
-    public void testDefaultArgument() {
-        // Passing no arguments should work.
-        String[] args = {};
-        HelloApp.main(args);
-    }
-
-    @Test
-    public void classInstanceForCodeCoverageTest() {
-        // Strictly speaking this test doesn't achieve anything, because HelloApp contains only a single static
-        // method, but for purposes of full code coverage it is included. In general,
-        // it is easier to aim for full code coverage and be done with it, than to remember why class X is stuck at
-        // 95% code coverage.
-        new HelloApp();
-    }
 }
